be1387d958b31a42801cb8206fb639c49915147e
Thiago Magalhães <thiagomagalhaes@ifactorys-macbook-pro.local>
2017-03-08 00:24:47 -0300

    Corrigindo um erro no umbrella.

--- TrackManager/TrackManager.h
+++ TrackManager/TrackManager.h
@@ -15,5 +15,3 @@ FOUNDATION_EXPORT double TrackManagerVersionNumber;
 FOUNDATION_EXPORT const unsigned char TrackManagerVersionString[];
 
 // In this header, you should import all the public headers of your framework using statements like #import
-
-#import "TrackManager-Swift.h"
